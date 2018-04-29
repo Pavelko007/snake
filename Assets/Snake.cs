a06a0d63ee07c6ec296fde8d3282f7f6b63e5c5c
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-29 15:40:46 +0300

    create snake and block prefabs

---
+++ Assets/Snake.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public GameObject SnakeSegment;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
