345f7e62c9e37b3fe8dee430905f56c3adddcca8
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-29 15:59:36 +0300

    basic snake movement

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     public GameObject SnakeSegment;
+    private GameObject snakeSegment;
+    private float lastStepTime;
+    private float stepDuration = .5f;
 
-	// Use this for initialization
+    void Awake()
+    {
+        lastStepTime = Time.time;
+    }
+    // Use this for initialization
 	void Start ()
 	{
-	    GameObject snakeSegment = Instantiate(SnakeSegment);
-        snakeSegment.transform.SetParent(transform);
+	    snakeSegment = Instantiate(SnakeSegment);
+	    snakeSegment.transform.SetParent(transform);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	    if (Time.time - lastStepTime > stepDuration)
+	    {
+	        snakeSegment.transform.Translate(Vector3.up);
+	        lastStepTime = Time.time;
+	    }
 		
 	}
 }
