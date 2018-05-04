1293c9d6ea9ce9635716e5cb1dc7123fdddaa8a7
Pavlo Kvas <kvaspasha@gmail.com>
2018-05-04 11:58:51 +0300

    add screen manager and game over panel

---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public GameObject StartScreen;
+
+    void Start()
+    {
+        ShowStartSceen();
+    }
+
+    public void ShowStartSceen()
+    {
+        HideAll();
+        StartScreen.SetActive(true);
+    }
+
+    private void HideAll()
+    {
+        Debug.LogError("not implemented");
+    }
+}
