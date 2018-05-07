1dc3260b4c2f700d24fd1a28ac9450dba6c133a1
Pavlo Kvas <kvaspasha@gmail.com>
2018-05-07 14:57:55 +0300

    add block script

---
+++ Assets/Block.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public Color DefaultColor;
+
+	// Use this for initialization
+	void Awake() {
+		ChangeColor(DefaultColor);
+	}
+
+    public void ChangeColor(Color color)
+    {
+        GetComponent<Renderer>().material.color = color;
+    }
+	
+}
